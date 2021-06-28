d18fcd25a1e19ac73be364d690c60c973db60668
wang90 <393210564@qq.com>
2021-06-28 23:41:30 +0800

    Update app.py

--- projectionist/app.py
+++ projectionist/app.py
@@ -12,17 +12,15 @@ from starlette.routing import Mount
 from starlette.applications import Starlette
 from starlette.config import Config
 
-# from projectionist.libs.sentry import SentryMiddleware
 from projectionist.config import (
     PROJECT_NAME,
 )
 
 app = FastAPI(title=PROJECT_NAME, openapi_url="/api/v1/openapi.json")
 
+# 跨域配置
 origins = [
     "http://localhost:8080",
-    "http://localhost:8081",
-    "http://localhost:8088",
 ]
 app.add_middleware(
     CORSMiddleware,
@@ -32,24 +30,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# app.add_middleware(SentryMiddleware)
 
 
 # 配置前端静态文件目录
-# app.mount("/js", StaticFiles(directory="projectionist/dist/js"), name="js")
-# app.mount("/css", StaticFiles(directory="projectionist/dist/css"), name="css")
-# app.mount("/img", StaticFiles(directory="projectionist/dist/img"), name="img")
 app.mount("/static", StaticFiles(directory="projectionist/static"), name="static")
 
 app.include_router(admin_router,prefix="/admin")
 app.include_router(api_router,prefix="/api")
 app.include_router(v1_router,prefix="/v1")
 app.include_router(web_router,prefix="/web")
-
-
-
-
-
-
-
 
